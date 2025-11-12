1e7ffb81dc75a18cd10ac40e1b7c5673b5d68d34
jazminh4308 <jazmin.h4308@gmail.com>
2025-11-12 13:12:29 -0800

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Student3 on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = " I Am A Programmer! "
+    var body: some View {
+        
+        VStack {
+            Spacer()
+
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            HStack {
+                Button(" Awsome ") {
+                    message = "Awesome!"
+                }
+                                
+                Button(" Great ") {
+                    message = "Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Student3 on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
